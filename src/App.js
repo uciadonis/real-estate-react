3e12af19690ebd1ef7cdb34ba2b5f1d10299ea5a
uciadonis <uciadonis@gmail.com>
2021-08-16 21:31:04 -0300

    Hero slider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import { SliderData } from "./data/SliderData";
+import GlobalStyle from "./globalStyles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+		<GlobalStyle />
+		<Navbar />
+		<Hero slides={ SliderData }/>
+    </>
   );
 }
 
---
+++ src/data/SliderData.js
@@ -0,0 +1,72 @@
+import ImageOne from "../images/house-1.jpg";
+import ImageTwo from "../images/house-2.jpg";
+import ImageThree from "../images/house-3.jpg";
+import ImageFour from "../images/house-4.jpg";
+
+export const SliderData = [
+    {
+        title: 'Luxury Villa Monte in Bali',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'Luxury Havana Miramar',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House Calabasas California',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },{
+        title: 'House Colonia Del Valle in CDMX',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    }
+]
+
+export const SliderDataTwo = [
+    {
+        title: 'Luxury Villa Monte in Vali',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'Luxury Villa Monte in Vali',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'Luxury Villa Monte in Vali',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },{
+        title: 'Luxury Villa Monte in Vali',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    }
+]
